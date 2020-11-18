91fc21033e42134beaf2120c01b73e700bdb6544
gonggongjohn <gonggongjohn@163.com>
2020-11-18 15:29:08 +0800

    initialize repo

---
+++ Calendar_CTR/Calendar_CTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_CTRApp.swift
+//  Calendar_CTR
+//
+//  Created by 龚敬洋 on 2020/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_CTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar_CTR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calendar_CTR
+//
+//  Created by 龚敬洋 on 2020/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
